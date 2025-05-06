41973f079f1734113a02cb55519ef6cec70bb5a7
herbgrace <thelen.alexander05@gmail.com>
2025-05-05 19:29:20 -0600

    Added Git Message to HelloWorld.cpp

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,11 @@
+// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "Git is now tracking this project!" << endl;
+    return 0;
+}
